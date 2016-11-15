066b10193358f6f4a98b18865e6f595a17fffa8b
秦世成 <qeesung@qq.com>
2016-11-15 11:38:04 +0800

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -60,8 +60,8 @@ module.exports = function(config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome','PhantomJS','Firefox'],
-
+        // browsers: ['Chrome','PhantomJS','Firefox'],
+         browsers: ['PhantomJS'],
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
